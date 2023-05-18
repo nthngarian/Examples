074bed8356cb5c49e649251b6b3b5c83c97c7992
Alexei <nthngarian@gmail.com>
2023-05-18 21:43:39 +0300

    created folder Example006

---
+++ Example006/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "юля")
+{
+    Console.WriteLine("Ура, это же ЮЛЯ!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.WriteLine(username);
+}
